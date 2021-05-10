1430859c2ccccafb0212c6066728b6688ec30f6e
Abraão Duarte <abraao.n.duarte@gmail.com>
2021-05-10 08:17:16 -0300

    feat: Configure eslint

--- src/infra/listen.ts
+++ src/infra/listen.ts
@@ -2,11 +2,9 @@ import * as http from 'http';
 import { Express } from 'express';
 
 function listen(handler: Express, { port = 80 } = {}): Promise<void> {
-  return new Promise((resolve, reject) => {
-    http.createServer(handler).listen(port)
-      .once('listening', resolve)
-      .once('error', reject);
-  });
+	return new Promise((resolve, reject) => {
+		http.createServer(handler).listen(port).once('listening', resolve).once('error', reject);
+	});
 }
 
 export { listen };
