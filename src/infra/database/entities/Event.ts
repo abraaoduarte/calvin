029c29335cc2a62ee132e56823bd62e91c116ad6
Abraão Duarte <abraao.n.duarte@gmail.com>
2021-07-01 19:42:20 -0300

    feat: Create Entity and migration

---
+++ src/infra/database/entities/Event.ts
@@ -0,0 +1,45 @@
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	UpdateDateColumn,
+	DeleteDateColumn,
+	CreateDateColumn,
+	OneToOne,
+	JoinColumn,
+} from 'typeorm';
+import { User } from './User';
+
+@Entity('events')
+export class Event {
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column('varchar')
+	title: string;
+
+	@Column('text')
+	description: string;
+
+	@Column('text')
+	location: string;
+
+	@CreateDateColumn({ type: 'datetime', name: 'date_start' })
+	dateStart: Date;
+
+	@CreateDateColumn({ type: 'datetime', name: 'date_end' })
+	dateEnd: Date;
+
+	@OneToOne(() => User, (user) => user.id)
+	@JoinColumn({ name: 'user_id' })
+	user: User;
+
+	@CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+	createdAt: Date;
+
+	@UpdateDateColumn({ type: 'timestamp', name: 'updated_at', nullable: true })
+	updatedAt: Date;
+
+	@DeleteDateColumn({ type: 'timestamp', name: 'deleted_at', nullable: true })
+	deletedAt: Date;
+}
