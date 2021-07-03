c1d0a4990f01c82a821f09bf9eca176f545b1f80
Abraão Duarte <abraao.n.duarte@gmail.com>
2021-07-03 10:45:22 -0300

    feat: Create schema validation for user

--- src/app/api/v1/user/user-router.ts
+++ src/app/api/v1/user/user-router.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import { CreateUserSchema, UpdateUserSchema } from 'domains/user/user-schema';
+import { validate } from 'app/middlewares/validate';
+import { makeUuidSchema } from 'domains/common/common-schema';
 import * as controller from './user-controller';
 
 const base = Router();
 const router = Router();
 
 router.get('/', controller.index);
-router.get('/:uuid', controller.show);
-router.post('/', controller.create);
-router.patch('/:uuid', controller.update);
+router.get('/:uuid', validate.params(makeUuidSchema('uuid')), controller.show);
+router.post('/', validate.body(CreateUserSchema), controller.create);
+router.patch('/:uuid', validate.params(makeUuidSchema('uuid')), validate.body(UpdateUserSchema), controller.update);
 router.delete('/:uuid', controller.destroy);
 
 base.use('/users', router);
